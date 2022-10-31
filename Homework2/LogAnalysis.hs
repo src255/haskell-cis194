0340dd26f2f03bd9b99b88fae4c7c170f7848ead
Christian Salvati <81280761+src255@users.noreply.github.com>
2022-10-30 20:36:31 -0400

    Use Parsec for parsing

--- Homework2/LogAnalysis.hs
+++ Homework2/LogAnalysis.hs
@@ -3,45 +3,39 @@
 module LogAnalysis where
 
 import Log
+import Text.ParserCombinators.Parsec as P
 
--- getNum :: String -> (Maybe Int, String)
--- getNum s = let details = break(== ' ') s
---            in case details of
---                 (x, ' ':xs) -> (readMaybe x, xs)
---                 _invalidLogMessage           -> (Nothing, "")
-
--- parseMessage :: String -> LogMessage
--- parseMessage text@('I':' ':rest) =  case time of
---     Nothing -> Unknown text
---     Just x  -> LogMessage Info x message
---     where
---         (time, message) = getNum rest
--- parseMessage text@('W':' ':rest) = case time of
---     Nothing -> Unknown text
---     Just x  -> LogMessage Warning x message
---     where
---         (time, message) = getNum rest
--- parseMessage text@('E':' ':rest) = case severity of
---     Nothing -> Unknown text
---     Just sev -> case time of
---         Nothing -> Unknown text
---         Just t  -> LogMessage (Error sev) t message
---     where
---         (severity, details) = getNum rest
---         (time, message) = getNum details
--- parseMessage text = Unknown text
+messageParser :: GenParser Char st LogMessage
+messageParser =
+  do
+    a <- oneOf "IW"
+    _ <- space
+    time <- read <$> many1 digit
+    _ <- space
+    msg <- manyTill anyChar eof
+    return
+      ( case a of
+          'I' -> LogMessage Info time msg
+          'W' -> LogMessage Warning time msg
+      )
+    <|> do
+      _ <- char 'E'
+      _ <- space
+      err <- read <$> many1 digit
+      _ <- space
+      time <- read <$> many1 digit
+      _ <- space
+      msg <- manyTill anyChar eof
+      return (LogMessage (Error err) time msg)
+    <|> do
+      text <- manyTill anyChar eof
+      return (Unknown text)
 
 parseMessage :: String -> LogMessage
-parseMessage text@('I' : ' ' : rest) = case words rest of
-    (time : message) -> LogMessage Info (read time) (unwords message)
-    _invalidLogMessage -> Unknown text
-parseMessage text@('W' : ' ' : rest) = case words rest of
-    (time : message) -> LogMessage Warning (read time) (unwords message)
-    _invalidLogMessage -> Unknown text
-parseMessage text@('E' : ' ' : rest) = case words rest of
-    (severity : time : message) -> LogMessage (Error (read severity)) (read time) (unwords message)
-    _invalidLogMessage -> Unknown text
-parseMessage text = Unknown text
+parseMessage s =
+  case P.parse messageParser "Error" s of
+    Right msg -> msg
+    Left _ -> Unknown ""
 
 parse :: String -> [LogMessage]
 parse = map parseMessage . lines
@@ -50,11 +44,14 @@ insert :: LogMessage -> MessageTree -> MessageTree
 insert (Unknown _) t = t
 insert message Leaf = Node Leaf message Leaf
 insert message (Node _ (Unknown _) _) = Node Leaf message Leaf
-insert message@(LogMessage _ time _) (Node left root@(LogMessage _ rootTime _) right)
+insert
+  message@(LogMessage _ time _)
+  (Node left root@(LogMessage _ rootTime _) right)
     | time < rootTime = Node (insert message left) root right
     | time > rootTime = Node left root (insert message right)
-    -- overwrite LogMessage if timestamp is equal
-    | otherwise = Node left message right
+    | -- overwrite LogMessage if timestamp is equal
+      otherwise =
+      Node left message right
 
 build :: [LogMessage] -> MessageTree
 build = foldr insert Leaf
@@ -68,11 +65,11 @@ whatWentWrong messages = map getMessage sortedErrorMessages
   where
     getMessage :: LogMessage -> String
     getMessage (LogMessage _ _ message) = message
-    getMessage _noMessage = ""
+    getMessage (Unknown _) = ""
 
     sortedErrorMessages = inOrder $ build $ filter isSevere messages
       where
         isSevere :: LogMessage -> Bool
         isSevere (LogMessage (Error severity) _ _)
-            | severity > 50 = True
+          | severity > 50 = True
         isSevere _notSevere = False
